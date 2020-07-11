ac76820b84ce8f48a81039400c58d3cf2f68831c
Nicholas (Hoy) Perell <nicholas.hoy@gmail.com>
2020-07-11 05:44:15 -0400

    Made a basis for the land mine powerup. Script for launching the powerup also works as a generic projectile shooting script.

---
+++ GMTK 2020 Jam/Assets/LandmineController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandmineController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float radiusOfTrigger;
+    public float startingVelocity;
+    public float timeThrown;
+    bool armed { get { return timeThrown < 0; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * startingVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!armed)
+        {
+            timeThrown -= Time.deltaTime;
+            if(armed)
+            {
+                rb.velocity = Vector3.zero;
+                //Some other indicator of being armed besides stopping
+            }
+        }
+        else
+        {
+            CheckForEnemy();
+        }
+    }
+
+    void CheckForEnemy()
+    {
+        foreach (Collider2D obj in Physics2D.OverlapCircleAll(transform.position, radiusOfTrigger))
+        {
+            if (obj.tag == "Enemy")
+            {
+                GoOff();
+            }
+        }
+    }
+
+
+    void GoOff()
+    {
+        //TODO
+        Debug.Log("Bomb Triggered By Enemy");
+        Destroy(this.gameObject);
+    }
+}
---
+++ GMTK 2020 Jam/Assets/LandminePower.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandminePower : MonoBehaviour
+{
+    public WizardController wizardController;
+    public GameObject landminePrefab;
+    public float manaCost;
+    public float timeToThrow;
+    public KeyCode key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!wizardController.possessed && !wizardController.busy && manaCost <= wizardController.currentMana)
+        {
+            CheckForInput();
+        }
+    }
+
+    void CheckForInput()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            ThrowTrap();
+        }
+    }
+
+    void ThrowTrap()
+    {
+        wizardController.currentMana -= manaCost;
+        Instantiate(landminePrefab, transform.position, transform.rotation);
+    }
+
+    IEnumerator BusyTime()
+    {
+        wizardController.busy = true;
+        yield return timeToThrow;
+        wizardController.busy = false;
+    }
+}
