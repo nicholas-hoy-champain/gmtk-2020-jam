730d5b61d13c506020b594382edb51521e9780b2
DarkBlade909 <52094720+DarkBlade909@users.noreply.github.com>
2020-07-14 13:00:14 -0500

    Anim fixes
    Fixed Transition from UpMoving to LeftMoving
    Renamed playerLeft
    Added a PixelGridLock script

---
+++ GMTK 2020 Jam/Assets/Scripts/PixelGrid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelGrid : MonoBehaviour
+{
+    [SerializeField]
+
+    private int pixelsPerUnit = 16;
+
+    private Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        Vector3 newLocalPosition = Vector3.zero;
+
+        newLocalPosition.x = (Mathf.Round(parent.position.x * pixelsPerUnit) / pixelsPerUnit) - parent.position.x;
+        newLocalPosition.y = (Mathf.Round(parent.position.y * pixelsPerUnit) / pixelsPerUnit) - parent.position.y;
+
+        transform.localPosition = newLocalPosition;
+    }
+}
